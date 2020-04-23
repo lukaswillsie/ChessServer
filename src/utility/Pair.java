5628b42f22dd823713eef57c4821235abc453185
Lukas <lukaswillsie@gmail.com>
2020-04-23 14:32:25 -0300

    Pair now implements Comparable; pairs are sorted primarily in increasing order of first element and secondarily in increasing order of second element

--- src/utility/Pair.java
+++ src/utility/Pair.java
@@ -1,6 +1,6 @@
 package utility;
 
-public class Pair {
+public class Pair implements Comparable<Pair> {
 	private int firstElement;
 	private int secondElement;
 	
@@ -27,5 +27,22 @@ public class Pair {
 	
 	public String toString() {
 		return "(" + this.firstElement + ", " + this.secondElement + ")";
+	}
+	
+	public boolean equals(Object obj) {
+		return obj instanceof Pair && (this.first() == ((Pair)obj).first() && this.second() == ((Pair)obj).second());
+	}
+
+	@Override
+	public int compareTo(Pair o) {
+		if(this.first() < o.first() || (this.first() == o.first() && this.second() < o.second())) {
+			return -1;
+		}
+		else if (this.equals(o)) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
 	}
 }
