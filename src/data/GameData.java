9fc52ab46bc7fe49debfde1b2dddcc18f6a6e75a
Lukas <lukaswillsie@gmail.com>
2020-05-06 20:47:56 -0300

    Added documentation for and code to support the features of offering draws, forfeiting, and archiving games

--- src/data/GameData.java
+++ src/data/GameData.java
@@ -14,17 +14,19 @@ public enum GameData {
 	BLACK("", 2, 's'),				// Name of the user playing black
 	STATE("0", 3, 'i'),				// State of the game; 0 if white's turn, 1 if black's turn
 	TURN("1", 4, 'i'),				// Current turn number
-	WHITE_CHK("0", 5, 'i'),			// 1 if white is in check, 0 if not
-	WHITE_CHKMT("0", 6, 'i'),		// 1 if white has been checkmated, 0 if not
-	BLACK_CHK("0", 7, 'i'),			// 1 if black is in check, 0 if not
-	BLACK_CHKMT("0", 8, 'i'),		// 1 if black has been checkmated, 0 if not
-	PROMOTION_NEEDED("0", 9, 'i');	// 1 if the player whose turn it is needs to promote a piece before their turn can end
+	WHITE_ARCHIVED("0", 5, 'i'),	// 1 if the white user has archived this game, 0 otherwise
+	BLACK_ARCHIVED("0", 6, 'i'),	// 1 if the black user has archived this game, 0 otherwise
+	DRAW_OFFERED("0", 7, 'i'),		// 1 if a draw has been offered, and the player whose turn it is
+									// needs to respond
+	DRAWN("0", 8, 'i'),				// 1 if the players in this game have agreed to a draw, 0 otherwise
+	WINNER("", 9, 's'),				// Contains the name of the winner, or the empty String if there isn't a winner
+	WHITE_CHK("0", 10, 'i'),		// 1 if white is in check, 0 if not
+	BLACK_CHK("0", 11, 'i'),		// 1 if black is in check, 0 if not
+	PROMOTION_NEEDED("0", 12, 'i');	// 1 if the player whose turn it is needs to promote a piece before their turn can end
 	
-	// This array contains every GameData value, in the order in which they should be sent
-	// to clients when sending GameData. We choose to standardize this order here, rather
-	// than hard-coding it elsewhere in the program or just using GameData.values() so
-	// that it's unequivocally defined somewhere.
-	public static final GameData[] order = {GAMEID, WHITE, BLACK, STATE, TURN, WHITE_CHK, WHITE_CHKMT, BLACK_CHK, BLACK_CHKMT, PROMOTION_NEEDED};
+	// This array contains all the GameData values, in the order in which they should be sent to clients,
+	// and the order in which they should appear in the active_games.csv file
+	public static final GameData[] order = {GAMEID, WHITE, BLACK, STATE, TURN, WHITE_ARCHIVED, BLACK_ARCHIVED, DRAWN, WINNER, WHITE_CHK, BLACK_CHK, PROMOTION_NEEDED};
 	
 	// The initial value that this piece of GameData should take upon creation of a new game
 	private String initial;
